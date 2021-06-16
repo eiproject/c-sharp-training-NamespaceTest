78564de7d11ee445af702465de9df6322a78b27a
Kosmas Suryopranoto <kosmas@formulatrix.com>
2021-06-16 18:39:47 +0700

    Bismillah and OK

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+//using CustomNameSpace;
+
+namespace Tutlane
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Welcome w = new Welcome();
+      CustomNameSpace.Welcome w = new CustomNameSpace.Welcome();
+      w.GreetMessage();
+      WriteLine("Press Any Key to Exit..");
+    }
+  }
+}
+
+namespace CustomNameSpace
+{
+  class Welcome
+  {
+    public void GreetMessage()
+    {
+      Console.WriteLine("Welcome to Tutlane");
+    }
+  }
+}
